04be79ddc718e255266a0c15765cc643de766ff9
Reuvain Neufeld <ryn4310@gmail.com>
2021-07-14 23:08:58 -0400

    captiol

--- client/parsha/src/App.js
+++ client/parsha/src/App.js
@@ -5,7 +5,7 @@ import DropDown from'./DropDown'
 function App() {
   return (
     <div className="App">
-      <h1 style={{color: 'red'}}>Welcome to our dvar-torah generator</h1>
+      <h1 style={{color: 'red'}}>Welcome to our Dvar-Torah generator</h1>
       <h4>Please pick a parsha</h4>
      <DropDown 
         onChange={e => console.log(e.target.value)}
